7ca1a2dabf6b7c2cffeec38b0319a76a60eb75be
Martin <david.martin6@ascension.org>
2024-04-28 10:21:14 -0500

    added logic to create comprehensive provider and dept dictionaries

--- taorouting.py
+++ taorouting.py
@@ -1,14 +1,25 @@
 import pandas as pd
 
-from utilities import fill_na_with_all,add_departments, add_providers,add_orders;
+from utilities import fill_na_with_all,create_routing_table,add_departments_and_providers;
 
 taos_raw_data = pd.read_csv('TAO.csv')
+providers = {}
+departments = {}
+
+columns = ['CONTEXT_ID', 'CONTEXT_NAME', 'TAO ID', 'Last Modified Date',
+       'Document Class/Subclass', 'Document Class/Subclass ID', 'Ordering',
+       'Action', 'Next Status', 'Department', 'Department ID', 'Provider',
+       'Provider ID', 'Order Type', 'Order Type ID', 'Order Genus',
+       'Assigned To', 'depts', 'providers', 'orders']
+
+athena_data_frame = pd.DataFrame(columns=columns)
 
 tao_fl = taos_raw_data[taos_raw_data['CONTEXT_NAME'] == 'FL - Ascension - Florida']
 tao_fl = fill_na_with_all(tao_fl)
 
-tao_fl = add_departments(tao_fl)
-tao_fl = add_providers(tao_fl)
-tao_fl = add_orders(tao_fl)
+departments, providers = add_departments_and_providers(tao_fl,departments,providers)
 
-print (tao_fl['orders'])
+
+print(departments)
+# print (tao_fl.columns)
+# print (create_routing_table(tao_fl))
--- utilities.py
+++ utilities.py
@@ -1,46 +1,71 @@
 
+import pandas as pd
+
+
 
 def fill_na_with_all(df):
     values = {"Provider ID": "All", "Order Type ID":"All", "Order Type":"All",}
     return df.fillna(value=values)
 
-def create_value_pairs(row, name_column, value_column):
-    depts = {}
-    print('row ', row)
-    if(row[name_column] == 'All'):
-        depts[name_column] = 'All'
-        return depts
+def update_dictionary_values(row, name_column, value_column,value_dict):
+    if(row[value_column] == 'All'):
+        value_dict[row[value_column]] = 'All'
+        return value_dict
     else:
-        department_names = row[name_column].split(',')
-        department_ids = row[value_column].split(',')
-        # print('department names ', department_names)
-        for x, y in zip(department_names, department_ids):
-            # print('dept', x)
-            depts[x] = y
-            # print(depts)
-    return depts
+        names = row[name_column].split(',')
+        ids = row[value_column].split(',')
+        for x, y in zip(ids, names):
+            value_dict[x] = y
+    return value_dict
 
 
-def create_orders(row, name_column, value_column):
-    print('row ', row)
-    depts = {}
-    if(row[name_column] == 'All'):
-        depts[name_column] = 'All'
-        return depts
-    else:
-        depts[row[name_column]] = row[value_column]
-    return depts
+# def create_orders(row, name_column, value_column):
+#     # print('row ', row)
+#     depts = {}
+#     if(row[name_column] == 'All'):
+#         depts[value_column] = 'All'
+#         return depts
+#     else:
+#         depts[row[value_column]] = row[name_column]
+#     return depts
 
 
-def add_departments(df):
-    df['depts'] = df.apply(lambda row: create_value_pairs(row, 'Department', 'Department ID'), axis=1)
+# def add_departments(df):
+#     df['depts'] = df.apply(lambda row: create_value_pairs(row, 'Department', 'Department ID'), axis=1)
+#     return df
+
+
+
+
+def process_department_and_provider_row(row, depts, providers):
+    depts = update_dictionary_values(row, 'Department', 'Department ID',depts)
+    providers = update_dictionary_values(row, 'Provider', 'Provider ID',providers)
+    return depts, providers
+
+def add_departments_and_providers(df, depts, providers):
+    for index, row in df.iterrows():
+        depts, providers = process_department_and_provider_row(row, depts, providers)
+    return depts, providers
+
+# def process_routing_row(row):
+#     departments = process_routing_column(row['Department ID'])
+#     providers = process_routing_column(row['Provider ID'])
+    # for dept in departments:
+    #     for provider in providers:
+    #         new_row = row.copy()
+    #             print(new_row)
+    # return row
+
+
+    
+
+
+
+def create_routing_table (df):
+    # for index, row in df.iterrows():
+    #     process_routing_row(row)
     return df
 
-def add_providers(df):
-    df['providers'] = df.apply(lambda row: create_value_pairs(row, 'Provider', 'Provider ID'), axis=1 )
-    return df
+        
 
-def add_orders(df):
-    print('df', df['Order Type'])
-    df['orders'] = df.apply(lambda row: create_orders(row, 'Order Type', 'Order Type ID'), axis=1 )
-    return df
+
