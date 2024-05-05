742424f414529513473d722d3d524f66c6adb45d
Martin <david.martin6@ascension.org>
2024-05-05 13:41:01 -0500

    fixed new route

--- tao_rules.py
+++ tao_rules.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from utilities import fill_na_with_all, update_dictionary_values, update_column_values,get_unique_column_values
-
+import array as arr
 # Data for practice routing is incomplete so will not process for now
 practice_routing = 5533
 
@@ -9,13 +9,13 @@ practice_routing = 5533
 def add_new_route(df, tao_id, depts,dept_ids, providers, provider_ids,priority, genus, order_type, order_type_ids):
     new_row = df.iloc[0].copy()
     new_row['TAO ID'] = tao_id
-    new_row['Department']= depts.tolist()
-    new_row['Department ID'] = dept_ids.tolist()
+    new_row['Department']= depts
+    new_row['Department ID'] = dept_ids
     new_row['Dept_ID_List'] = dept_ids
-    new_row['Provider'] = providers.tolist()
-    new_row['Provider ID'] = provider_ids.tolist()
+    new_row['Provider'] = providers
+    new_row['Provider ID'] = provider_ids
     new_row['Ordering'] = priority
-    new_row['Order Genus'] = genus.tolist()
+    new_row['Order Genus'] = genus
     new_row['Genus_List'] = genus
     new_row['Order Type'] = order_type
     new_row['Order Type ID'] = order_type_ids
