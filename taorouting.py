b3ffa2b9736b270cacbb79c19c3778dd82268b59
Martin <david.martin6@ascension.org>
2024-05-05 09:36:23 -0500

    moved tao rules logic to its own file

--- taorouting.py
+++ taorouting.py
@@ -1,36 +1,18 @@
 import pandas as pd
 
-from utilities import fill_na_with_all,create_routing_table,get_genus, get_departments,update_routing_rows,add_departments_and_providers
+# from utilities import  get_departments,update_routing_rows,add_departments_and_providers
 from orders import order_dtypes, fill_orders_na_with_all, remove_comments,get_order_providers, get_order_genus,get_genus_orderIds
 from providers import get_providers
 from routing import route
-
-columns = ['CONTEXT_ID', 'CONTEXT_NAME', 'TAO ID', 'Last Modified Date',
-       'Document Class/Subclass', 'Document Class/Subclass ID', 'Ordering',
-       'Action', 'Next Status', 'Department', 'Department ID', 'Provider',
-       'Provider ID', 'Order Type', 'Order Type ID', 'Order Genus',
-       'Assigned To']
+from tao_rules import get_tao_rules
 
 
-taos_raw_data = pd.read_csv('TAO.csv')
 tao_orders = pd.read_csv('TAOOrders.csv', dtype=order_dtypes)
 
-providers = {}
-departments = {}
-genus = []
-order_providers = {}
-order_genus = {}
-order_buckets = {}
 
 
-athena_data_frame = pd.DataFrame(columns=columns)
+athena_toa_rules = get_tao_rules('TAO.csv','FL - Ascension - Florida')
 
-tao_fl = taos_raw_data[taos_raw_data['CONTEXT_NAME'] == 'FL - Ascension - Florida']
-tao_fl = fill_na_with_all(tao_fl)
-tao_fl = update_routing_rows(tao_fl)
-departments, providers = add_departments_and_providers(tao_fl,departments,providers)
-genus = get_genus(tao_fl)
-athena_data_frame = create_routing_table(tao_fl,departments,providers,athena_data_frame)
 
 tao_orders = fill_orders_na_with_all(tao_orders)
 tao_orders = remove_comments(tao_orders)
@@ -44,6 +26,6 @@ get_genus_orderIds(tao_orders)
 get_providers('fljac_providers.json')
 # print(athena_data_frame.columns)
 # print(athena_data_frame[athena_data_frame['Department ID'] == '768'])
-routes = route(athena_data_frame, 'All', 'All', 'All', 427181).sort_values(by='Ordering')
-print(routes[['TAO ID','Ordering','Assigned To']])
+# routes = route(athena_data_frame, 'All', 'All', 'All', 427181).sort_values(by='Ordering')
+# print(routes[['TAO ID','Ordering','Assigned To']])
 # print(athena_data_frame[athena_data_frame['TAO ID'] == 7787 ])
--- utilities.py
+++ utilities.py
@@ -19,22 +19,10 @@ def update_dictionary_values(row, name_column, value_column,value_dict):
     return value_dict
 
 
-def process_routing_column(value):
-    if value == 'All':
-        return ['All']
-    return value.split(',') 
 
 
-def process_department_and_provider_row(row, depts, providers):
-    depts = update_dictionary_values(row, 'Department', 'Department ID',depts)
-    providers = update_dictionary_values(row, 'Provider', 'Provider ID',providers)
-    return depts, providers
 
-def add_departments_and_providers(df, depts, providers):
-    for index, row in df.iterrows():
-        if(row['TAO ID'] != practice_routing):
-            depts, providers = process_department_and_provider_row(row, depts, providers)
-    return depts, providers
+
 
 
 
@@ -46,48 +34,29 @@ def update_column_values(row, value_column):
     return value_strip_list
 
 
-def update_routing_rows(df):
-    for index, row in df.iterrows():
-        if(row['TAO ID'] != practice_routing):
-            df['Dept_ID_List'] = df.apply(lambda row: update_column_values(row, 'Department ID'), axis=1)
-            df['Provider_ID_List'] = df.apply(lambda row: update_column_values(row, 'Provider ID'), axis=1)
-            df['Genus_List'] = df.apply(lambda row: update_column_values(row, 'Order Genus'), axis=1)
-    return df
+
+def get_unique_column_values(df, column_name):
+    cur_df = df[[column_name]]
+    sel_group = cur_df.groupby(by=['Order Genus'])
+    cur_values =[]
+    for name_of_group,contents_of_group  in sel_group:
+        cur_group = {'name': name_of_group[0]}
+        cur_values.append(cur_group)
+    return cur_values 
 
 
-
-def process_routing_row(row,depts, providers,df):
-    cur_depts = process_routing_column(row['Department ID'])
-    cur_providers = process_routing_column(row['Provider ID'])
-    for dept in cur_depts:
-        for provider in cur_providers:
-            if row['TAO ID'] == 5533:
-                continue
-            new_row = row.copy()
-            new_row['Provider ID'] = provider
-            new_row['Provider'] = providers[provider.strip()]
-            new_row['Department ID'] = dept
-            new_row['Department'] = depts[dept.strip()] 
-            df = pd.concat([pd.DataFrame([new_row]), df], ignore_index=True) 
-    return df
 
 
 # def get_genus(df):
-#     genus_list = []
-#     for index, row in df.iterrows():
-#         cur_row = row['Order Genus'].split(',')
-#         genus_list = set([*genus_list,*cur_row])
-#     genus_stripped_list = [j.strip() for j in genus_list]
-#     return genus_stripped_list
+#     genus_df = df[['Order Genus']]
+#     genus_group = genus_df.groupby(by=['Order Genus'])
+#     genus =[]
+#     for name_of_group,contents_of_group  in genus_group:
+#         cur_group = {'name': name_of_group[0]}
+#         genus.append(cur_group)
+#     return genus
 
-def get_genus(df):
-    genus_df = df[['Order Genus']]
-    genus_group = genus_df.groupby(by=['Order Genus'])
-    genus =[]
-    for name_of_group,contents_of_group  in genus_group:
-        cur_group = {'name': name_of_group[0]}
-        genus.append(cur_group)
-    return genus
+
 
 
 def get_departments(df):
@@ -98,13 +67,6 @@ def get_departments(df):
         cur_group = {'name': name_of_group[0], 'value': name_of_group[1] }
         depts.append(cur_group)
     return depts
-
-def create_routing_table (df, depts, providers,routing_df):
-    for index, row in df.iterrows():
-        if(row['TAO ID'] != practice_routing):
-            routing_df = process_routing_row(row,depts, providers,routing_df)
-    return routing_df
-
         
 
 
