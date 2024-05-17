b35c022a8edfa9d9579e3389b25e8a5e606d64ad
Martin <david.martin6@ascension.org>
2024-05-17 08:56:05 -0500

    fixed routing logic

--- routing.py
+++ routing.py
@@ -20,11 +20,9 @@ def route(df, dept_id, provider_id, genus, order_id):
         depts = df[(df['Department ID'].str.contains(dept_id)) | (df['Department ID'] == 'All')]
     if (provider_id != '0'):
         depts = depts[(depts['Provider ID'].str.contains(provider_id)) | (depts['Provider ID'] == 'All')]
-    print('dept 1', depts)
     if (genus != '0'):
         depts = depts[(depts['Genus_List'].str.contains(genus, regex=False)) |  (depts['Genus_List'].str.contains('All', regex=False))]
     if (order_id != '0'):
         depts = depts[(depts['Order Type ID'].str.contains(order_id,regex=False)) | (depts['Order Type ID'] == 'All')]
-    print('dept 4', depts)
     results = convert_results(depts.sort_values(by='Ordering'))
     return results
