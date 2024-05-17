fb42a4793e22586d68ab9737c8b953cef86549a6
Martin <david.martin6@ascension.org>
2024-05-17 08:55:41 -0500

    fixed routing logic

--- routing.py
+++ routing.py
@@ -14,24 +14,17 @@ def convert_results(df):
 
 def route(df, dept_id, provider_id, genus, order_id):
     
+    depts = df
+
     if (dept_id != '0'):
         depts = df[(df['Department ID'].str.contains(dept_id)) | (df['Department ID'] == 'All')]
-    else:
-        depts = df[df['Department ID'] == 'All']
     if (provider_id != '0'):
         depts = depts[(depts['Provider ID'].str.contains(provider_id)) | (depts['Provider ID'] == 'All')]
-    else:
-        depts = depts[depts['Provider ID'] == 'All']
     print('dept 1', depts)
     if (genus != '0'):
         depts = depts[(depts['Genus_List'].str.contains(genus, regex=False)) |  (depts['Genus_List'].str.contains('All', regex=False))]
-    else:
-        depts = depts[depts['Order Genus'] == 'All']
     if (order_id != '0'):
         depts = depts[(depts['Order Type ID'].str.contains(order_id,regex=False)) | (depts['Order Type ID'] == 'All')]
-    else:
-        depts = depts[depts['Order Type ID'] == 'All']
-    print('orderId', order_id)
-    print('dept 2', depts['Order Type ID'])
+    print('dept 4', depts)
     results = convert_results(depts.sort_values(by='Ordering'))
     return results
